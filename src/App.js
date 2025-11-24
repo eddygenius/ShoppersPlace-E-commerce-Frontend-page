17ca5dbb817c57f05ce388786338d533e01eb37b
Edward Nkemdirim <edwardnkem4@gmail.com>
2025-11-24 23:25:00 +0100

    made changes to app.js

--- src/App.js
+++ src/App.js
@@ -838,7 +838,7 @@ function Footer() {
           </div>
         </div>
         <div className="border-t border-gray-800 mt-8 pt-8 text-center text-gray-400">
-          <p>&copy; 2024 ShopHub. All rights reserved.</p>
+          <p>&copy; 2024 ShoppersPlace. All rights reserved.</p>
         </div>
       </div>
     </footer>
